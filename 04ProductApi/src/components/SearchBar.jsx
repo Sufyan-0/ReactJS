f59c97a96c62e9d8fa514e4f19697f6ff9a9df46
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-21 18:51:53 +0500

    input console working

--- 04ProductApi/src/components/SearchBar.jsx
+++ 04ProductApi/src/components/SearchBar.jsx
@@ -6,7 +6,9 @@ function SearchBar() {
        
     <div className="relative mt-2 rounded-md shadow-sm">
       <input
-      
+        onChange={(event)=>{
+          console.log(event.target.value)
+        }}
         type="search"
         placeholder="What are you looking for... "
         className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
