a6bc82cbe4b4a44ea399016e8b5df9922285b214
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-22 17:45:18 +0500

    search bar work done

--- 04ProductApi/src/App.jsx
+++ 04ProductApi/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-     <SearchBar></SearchBar>
+     {/* <SearchBar></SearchBar> */}
      <ProductCard></ProductCard>
     </>
   )
--- 04ProductApi/src/components/SearchBar.jsx
+++ 04ProductApi/src/components/SearchBar.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+  // console.log("props" , props.data)
+  const [userSearch , setUserSearch] = useState("")
+  
+  const handleSearch = () => {
+    onSearch(userSearch); // Pass the search query to the parent
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch(); // Trigger search on Enter key press
+    }
+  };
+
+
   return (
-       <div className="container mx-auto">
-
-       
-    <div className="relative mt-2 rounded-md shadow-sm">
-      <input
-        onChange={(event)=>{
-          console.log(event.target.value)
-        }}
-        type="search"
-        placeholder="What are you looking for... "
-        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-      />
+       <div className="container w-full mx-auto flex justify-center">   
+    <div className="relative mt-2 rounded-md shadow-sm item-center">
+    <input
+          value={userSearch}
+          onChange={(event) => setUserSearch(event.target.value)}
+          onKeyPress={handleKeyPress}
+          type="search"
+          placeholder="What are you looking for..."
+          className=" rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+        />
+        <button
+          onClick={handleSearch}
+          className="inline  ml-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Search
+        </button>
     </div>
     </div>
   )
