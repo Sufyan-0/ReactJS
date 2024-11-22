66f8bfe6c27f64d1f37724d03afb734de55ac56e
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-22 17:45:50 +0500

    working on filter

---
+++ 04ProductApi/src/components/Filter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Filter() {
+  return (
+    <div className=' container mx-auto w-full flex justify-center bg-green-500  my-5'>
+        <h2>filters here</h2>
+    </div>
+  )
+}
+
+export default Filter
